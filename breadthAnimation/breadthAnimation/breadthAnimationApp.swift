8ea0acbbaa764731aedfb07f98542815bbe175e0
Rutik Maraskolhe <84556881+Cretus-iosDev@users.noreply.github.com>
2021-07-27 19:12:38 +0530

    Add files via upload

---
+++ breadthAnimation/breadthAnimation/breadthAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  breadthAnimationApp.swift
+//  breadthAnimation
+//
+//  Created by rutik maraskolhe on 27/07/21.
+//
+
+import SwiftUI
+
+@main
+struct breadthAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
